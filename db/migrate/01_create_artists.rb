af34c17b74a98bc23183d9247347ddbfdf8e639a
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-29 03:26:22 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -10,7 +10,7 @@ class CreateArtists < ActiveRecord::Migration[5.2]
     create_table :artists do |t|
       t.string :name
       t.string :genre
-      t.integer :age
+      t.srting :age
       t.string :hometown
     end
   end
